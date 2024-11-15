9dae53a447cb6450d8e549ae9ad2a8edbee406cd
yoon322 <dbswjd0256@naver.com>
2024-11-15 21:40:47 +0900

    Revert "feat: trunk test case 추가"
    This reverts commit b3cceb8774889d7ce28002ebbd36637217ac4195.

--- main.py
+++ main.py
@@ -759,35 +759,6 @@ class TestTempLockSystem(unittest.TestCase):
         self.assertEqual(self.car_controller.get_right_door_status(), "CLOSED")
         self.assertEqual(self.car_controller.get_right_door_lock(), "LOCKED")  # 문이 닫히면 잠금 적용됨
 
-class TestCarControllerTrunk(unittest.TestCase):
-    def setUp(self):
-        self.car = Car()
-        self.car_controller = CarController(self.car)
-
-    def test_trunk_open_callback(self):
-        self.assertFalse(self.car.get_trunk_status())  # 초기 상태 확인
-        execute_command_callback("TRUNK_OPEN", self.car_controller)  # 트렁크 열기
-        self.assertTrue(self.car.get_trunk_status())  # 트렁크 열린 상태 확인
-
-    def test_trunk_close_directly(self):
-        self.car_controller.open_trunk()
-        self.assertTrue(self.car.get_trunk_status())  # 트렁크 열린 상태 확인
-        self.car_controller.close_trunk()
-        self.assertFalse(self.car.get_trunk_status())  # 트렁크 닫힘 상태 확인
-
-    def test_trunk_speed_limit_with_callback(self):
-        execute_command_callback("TRUNK_OPEN", self.car_controller)
-        self.assertTrue(self.car.get_trunk_status())
-        self.car.set_speed(40)
-        execute_command_callback("ACCELERATE", self.car_controller)  # 트렁크 열린 상태에서 가속 시도
-        self.assertEqual(self.car.get_speed(), 40)  # 속도 유지 확인
-
-    def test_trunk_speed_allowance_below_limit_with_callback(self):
-        execute_command_callback("TRUNK_OPEN", self.car_controller)
-        self.assertTrue(self.car.get_trunk_status())
-        self.car.set_speed(20)
-        execute_command_callback("ACCELERATE", self.car_controller)  # 트렁크 열린 상태에서 제한 속도 이내 가속 시도
-        self.assertEqual(self.car.get_speed(), 30)  # 속도가 10 증가했는지 확인
 
 class TestCarTrunk(unittest.TestCase):
     def setUp(self):
